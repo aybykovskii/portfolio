182719468c055e2f72e9c9001aad989c8386d84c
aybykovskii <aybykovskii@gmail.com>
2026-04-25 03:07:11 +0700

    Оптимизация

--- apps/web/src/components/Bar.tsx
+++ apps/web/src/components/Bar.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export const Bar = ({ title, progress }: Props) => (
   <div className="w-full flex flex-col gap-2">
-    <h3 className="text-sm font-medium text-base-content">{title}</h3>
+    <p className="text-sm font-medium text-base-content">{title}</p>
     <progress className="progress progress-primary w-full" value={progress} max={100} />
   </div>
 )
